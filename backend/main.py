a0cd6f806473005060f22693b425043141b5a25b
VayerMaking <martin.vayer@gmail.com>
2022-10-19 11:48:04 +0200

    setup tiny db and integration with routes

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,43 @@
 # flask server
-
+from tinydb import TinyDB, Query
 from datetime import datetime
 from flask import Flask, render_template, request
  
 app = Flask(__name__) 
+db = TinyDB("db.json")
+Order = Query()
  
-@app.route("/") 
-def index(): 
-    return render_template('index.html')
+@app.route('/orders', methods=['POST'])
+def makeOrder():
+    order = request.get_json()
+    db.insert(order)
+    return 'ok'
+
+@app.route('/orders', methods=['GET'])
+def getOrders():
+    orders = db.all()
+    return orders
+
+def removeFinishedOrders():
+    db.remove(Order.status == 'completed')
+    return "ok"
+
+# @app.route("/") 
+# def index(): 
+#     return render_template('index.html')
 
 @app.route("/oven_start", methods=['POST'])
 def oven_start():
-    print(request.json)
+    db.update({'status': 'in the oven'}, Order.status == 'preparation')
     return "ok"
 
-@app.route("/tracker", methods=['GET'])
-def tracker(): 
-    return render_template('tracker.html')
+# @app.route("/tracker", methods=['GET'])
+# def tracker(): 
+#     return render_template('tracker.html')
 
-@app.route("/cashier", methods=['GET']) 
-def cashier(): 
-    return render_template('cashier.html')
+# @app.route("/cashier", methods=['GET']) 
+# def cashier(): 
+#     return render_template('cashier.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
